2b3bf21601c776985a28839db04213bb37c95152
Vera0011 <contact.vera0011@gmail.com>
2023-10-31 07:35:51 +0100

    Creating characters

---
+++ Assets/Scripts/CheckGround.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public static bool isGrounded;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        isGrounded = false;
+    }
+}
---
+++ Assets/Scripts/collissions_manager.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collissions_manager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
